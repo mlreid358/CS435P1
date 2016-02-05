b2b5ee8b19cc34cf9340be05a05a8e0dbb30a512
Mike Reid <mlreid358@gmail.com>
2016-02-04 17:51:09 -0700

    third sync

--- src/ngram.java
+++ src/ngram.java
@@ -1,6 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ngram {
-	ngram(String s, int n){
-		
+	private String sentence = "";
+	private int n = 0;
+	private String author = "";
+	private String year = "";
+	private ArrayList<String> grams = new ArrayList<String>(10000);
+	private static final boolean DEBUG = false;
+	ngram(String s, int n, String a, String y){
+		this.sentence = s;
+		this.author = a;
+		this.year =y;
+		this.n = n;
+		if(DEBUG){
+			System.out.println("Author: "+this.author+"\nYear: "+this.year+"\n"+sentence);
+		}
+		Scanner scanny = new Scanner(sentence);
+		if(n==1){//unigram
+			if(!scanny.hasNext()) return;
+			scanny.next();
+			String temp = "";
+			while(scanny.hasNext()){
+				temp = scanny.next();
+				if(!(temp.equals("_START_")||temp.equals("_END_")))
+					grams.add(temp+"\t"+this.author+"\t"+this.year);
+			}
+		}else{//ngram == 2
+			if(!scanny.hasNext()) return;
+			String temp ="";
+			String old = scanny.next();
+			while(scanny.hasNext()){
+				temp = scanny.next();
+				if(!(old.equals("_END_")&&temp.equals("_START_") || old.equals("_START_")&&temp.equals("_END_")))
+					grams.add(old+" "+temp+"\t"+this.author+"\t"+this.year);
+				old = temp;
+			}
+		}//could achieve ngram with nested for loop
+	}
+	public String toString(){
+		String out = "";
+		for(int i = 0; i<this.grams.size(); i++){
+			out+=this.grams.get(i)+"\n";
+		}
+		return out;
+	}
+	public void writeOut(final String fn){
+		try {
+			PrintWriter pw = new PrintWriter( new File(fn));
+			for(int i = 0; i<this.grams.size(); i++){
+				pw.println(this.grams.get(i));}
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/task.java
+++ src/task.java
@@ -1,57 +1,80 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-	
+
 
 public class task {
 	private static String line = "";
-	private static boolean DEBUG = true;
+	private static boolean DEBUG = false;
+	private static final String dataPath = "/home/michael/workspace/workspace/CS_435_P1/sampleData/";
+	//"/s/bach/d/under/mikereid/cs435/CS435P1/sampleData/";
 	public static void main(String[] args) {
 		if(args.length!=0 && args[args.length-1].substring(0, 1).toLowerCase().equals("d")){
 			DEBUG = true;
 		}
 		try {
-			File folder = new File("/home/michael/workspace/workspace/CS_435_P1/sampleData");
+			File folder = new File(dataPath);
 			String[] files = folder.list();
-			System.out.println("files: ");
-			for(int i = 0; i<files.length; i++){
-				System.out.println("#"+i+" "+files[i]);
+			if(DEBUG){
+				System.out.println("files: ");
+				for(int i = 0; i<files.length; i++){
+					System.out.println("#"+i+" "+files[i]);
+				}
 			}
-			Scanner scanny = new Scanner(new File("sample2.txt"));
-			String peek = "";
-			String author = "";
-			String year = "";
-			while(scanny.hasNextLine()){
-				peek = scanny.nextLine();
-				if(peek.length()>6 && peek.substring(0,7).equals("Author:")){
-					int i = peek.length()-1;
-					while (true){
-						if(peek.charAt(i) == ' ') break;
-						i--;
+			
+			for(int j = 0; j<files.length; j++){
+				System.out.println("#"+j+" "+files[j]);
+				Scanner scanny = new Scanner(new File(dataPath+files[j]));
+				String peek = "";
+				String author = "";
+				String year = "";
+				while(scanny.hasNextLine()){
+					peek = scanny.nextLine().trim();
+					//parse author
+					if(peek.length()>6 && peek.substring(0,7).equals("Author:")){
+						int i = peek.length()-1;
+						while (true){
+							if(peek.charAt(i) == ' ') break;
+							i--;
+						}
+						author = peek.substring(i+1, peek.length());
+						if(DEBUG)System.out.println(author);
+					}//parse year
+					if(peek.length()>13 && peek.substring(0,13).equals("Release Date:")){
+						if(peek.substring(peek.length()-1,peek.length()).equals("]")){
+													//-8 [EBOOK #FN (-4: rem .txt)
+							year = peek.substring(0,peek.length()-9-(files[j].trim().length()-4));
+							year = year.trim().substring(year.trim().length()-4, year.trim().length());
+						}else{ //year is last 4 of trimmed string
+							year = peek.substring(peek.length()-4, peek.length());
+						}
+						if(DEBUG)System.out.println(year);
 					}
-					author = peek.substring(i+1, peek.length());
-				}if(peek.length()>13 && peek.substring(0,13).equals("Release Date:")){
-					year = peek.substring(peek.length()-4, peek.length());
-				}
-				if(peek.length()>41 && peek.substring(0,41).equals("*** START OF THIS PROJECT GUTENBERG EBOOK")){
-					while(true){
-						peek=scanny.nextLine();
-						if(peek.substring(0,7).equals("*** END")){
-							break;
-						}
-						line+=peek+" ";
-						if(DEBUG){
-							System.out.println(year);
-							System.out.println(author);
-							System.out.println(line);
-						}
-						clean();
-					}//end while
-				}
-			}				
-			scanny.close();
+					
+					//parse book
+					if(peek.length()>41 && peek.substring(0,41).equals("*** START OF THIS PROJECT GUTENBERG EBOOK")){
+						while(scanny.hasNextLine()){
+							peek=scanny.nextLine();
+							if(peek.length()>7 && peek.substring(0,7).equals("*** END")){
+								break;
+							}
+							line+=peek+" ";
+							if(DEBUG){
+								System.out.println(year);
+								System.out.println(author);
+								System.out.println(line);
+							}
+							
+						}//end while
+					}
+				}clean();
+				ngram grams1 = new ngram(line,1,author,year);
+				ngram grams2 = new ngram(line,2,author,year);
+				grams2.writeOut("test.txt");
+				//System.out.println(grams);
+				scanny.close();
+			}		
 		}//end try
 		catch (FileNotFoundException e) {
 			System.out.println("File "+args[0]+" not found.");
@@ -62,7 +85,7 @@ public class task {
 		String cleaned = "_START_ ";
 		char c = '\n';
 		line = line.toLowerCase();
-		ArrayList<String> sents = new ArrayList<String>();
+		String sents = "";
 		for(int i = 0; i<line.length(); i++){
 			c = line.charAt(i);
 			//strip all miscellaneous characters
@@ -70,16 +93,17 @@ public class task {
 				cleaned+=c;
 			}
 			if((c == '.' || c == '!' || c == '?') && cleaned.length()>8){
-				cleaned+=" _END_";
-				sents.add(cleaned);
+				cleaned+=" _END_ ";
+				sents+=(cleaned);
 				cleaned="_START_ ";
 			}
 		}
 		if(DEBUG) {
-			for(int i = 0; i<sents.size(); i++){
-				System.out.println(sents.get(i));
-			}
-		}
+			
+				System.out.println(sents);
+			
+		}line = sents;
 		return;
 	}
+	
 }
